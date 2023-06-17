5a1447a002e240e35e64d920738731b17c2797a2
MichaelShults <mishults@gmail.com>
2023-06-17 22:21:05 +0300

    Setting project structure

---
+++ app/database/knexfile.js
@@ -0,0 +1,18 @@
+const path = require('path');
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+        directory: __dirname + "/../../migrations",
+        tableName: 'migrations'
+      },
+  },
+  
+};
---
+++ migrations/20230617190904_first-migration.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('myTable', function(table){
+    table.increments('id');
+    table.string('data').notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('myTable');
+};
